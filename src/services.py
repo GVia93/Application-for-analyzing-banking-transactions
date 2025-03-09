4fc914108e50998b89bde4d6cb8618ab63c809c0
gvia <gritcaev.vv@gmail.com>
2025-03-09 22:11:01 +0300

    add logger

--- src/services.py
+++ src/services.py
@@ -1,19 +1,28 @@
 import json
+import logging
 import re
 
 import pandas as pd
 
-from src.utils import file_reader
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler("logs/services.log", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
-def find_p2p_transfers(data) -> str:
+def find_p2p_transfers(data) -> str | None:
     """
     Функция находит переводы физическим лицам.
     Возвращает JSON со всеми такими транзакциями.
     """
-    df = pd.DataFrame(data)
-    pattern = re.compile(r"^[А-ЯЁ][а-яё]+\s[А-ЯЁ]\.$")
-    p2p_transfers = df[(df["Категория"] == "Переводы") & df["Описание"].str.match(pattern)]
-
-    result = p2p_transfers.to_dict(orient="records")
-    return json.dumps(result, ensure_ascii=False, indent=2)
+    try:
+        df = pd.DataFrame(data)
+        pattern = re.compile(r"^[А-ЯЁ][а-яё]+\s[А-ЯЁ]\.$")
+        p2p_transfers = df[(df["Категория"] == "Переводы") & df["Описание"].str.match(pattern)]
+        result = p2p_transfers.to_dict(orient="records")
+        return json.dumps(result, ensure_ascii=False, indent=2)
+    except Exception as e:
+        logger.error(f"Ошибка при поиске P2P переводов: {e}")
+        return None
