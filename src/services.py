aa7b08082203a4c92ecd6d5ded4ded531977883d
gvia <gritcaev.vv@gmail.com>
2025-03-10 11:52:17 +0300

    add test_services + fix

--- src/services.py
+++ src/services.py
@@ -12,17 +12,26 @@ file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
-def find_p2p_transfers(data) -> str | None:
+def find_p2p_transfers(df: pd.DataFrame) -> str | None:
     """
     Функция находит переводы физическим лицам.
     Возвращает JSON со всеми такими транзакциями.
     """
     try:
-        df = pd.DataFrame(data)
+        if not isinstance(df, pd.DataFrame):
+            logger.error(f"Не корректный формат данных.")
+            raise ValueError()
+
         pattern = re.compile(r"^[А-ЯЁ][а-яё]+\s[А-ЯЁ]\.$")
-        p2p_transfers = df[(df["Категория"] == "Переводы") & df["Описание"].str.match(pattern)]
-        result = p2p_transfers.to_dict(orient="records")
-        return json.dumps(result, ensure_ascii=False, indent=2)
+        p2p_transfers = df[(df["Категория"] == "Переводы") & df["Описание"].str.match(pattern, na=False)]
+
+        if not p2p_transfers.empty:
+            result = p2p_transfers.to_dict(orient="records")
+            return json.dumps(result, ensure_ascii=False, indent=2)
+
+        logger.info("Нет подходящих P2P переводов.")
+        return json.dumps([], ensure_ascii=False, indent=2)
+
     except Exception as e:
         logger.error(f"Ошибка при поиске P2P переводов: {e}")
         return None
